f20ace6be82803c68bde5ce02b4c68bde1f527dc
tylerforajter <100237457+tylerforajter@users.noreply.github.com>
2023-12-28 11:29:18 -0500

    Update Collection.js

--- Api/Controllers/Collection.js
+++ Api/Controllers/Collection.js
@@ -10,15 +10,6 @@
 
 const collectionService = require('../Services/mysql/collectionService')
 
-module.exports.createCollection = async function createCollection(req, res, next){
-        res.status(201).json({message: "createCollection Method Called successfully"})
-
-
-
-}
-
-
-
 
 module.exports.getCollection = async function getCollection(req, res, next){
         let userName = req.params.userName;
@@ -33,7 +24,7 @@ module.exports.getCollection = async function getCollection(req, res, next){
 module.exports.getCollectionPoamStats = async function getCollectionPoamStats(req, res, next){
         //let collectionId = req.params.collectionId
         //console.log(userName)
-        console.log("getCollectionPoamStats")
+        // console.log("getCollectionPoamStats")
 
         var getCollection = await collectionService.getCollectionPoamStats(req, res, next)
 
@@ -44,7 +35,7 @@ module.exports.getCollections = async function getCollections(req, res, next){
 
 
 	let userName = req.query.userName
-        console.log("getcollections: ", userName)
+        // console.log("getcollections: ", userName)
         
 
 	var getCollections = await collectionService.getCollections(userName)
@@ -52,47 +43,20 @@ module.exports.getCollections = async function getCollections(req, res, next){
 	res.status(201).json(getCollections)
 }
 
+module.exports.postCollection = async function postCollection(req, res, next){
+        //res.status(201).json({message: "postPoamApprover Method called successfully"});
+        var collection = await collectionService.postCollection(req,res,next); 
+        //console.log("returning poamApprover: ", poamApprover)
+        res.status(201).json(collection)
+}
 
 module.exports.putCollection = async function putCollection(req, res, next){
-        console.log("putCollection()...")
+        // console.log("putCollection()...")
         var collection = await collectionService.putCollection(req,res,next); 
         res.status(201).json(collection)
 }
 
-module.exports.replaceCollection = async function replaceCollection(req, res, next){
-        res.status(201).json({message: "replaceCollection Method called successfully"})
-}
-
 module.exports.deleteCollection = async function deleteCollection(req, res, next){
         res.status(201).json({message: "deleteCollection Method called successfully"})
-}
-
-
-module.exports.getPOAMByCollection = async function getPOAMByCollection(req, res, next){
-        res.status(201).json({message: "getPOAMByCollection Method called successfully"})
-}
-
-
-
-module.exports.getPOAM_AssetsByCollectionUser = async function getPOAM_AssetsByCollectionUser(req, res, next){
-        res.status(201).json({message: "getPOAM_AssetsByCollectionUser Method called successfully"})
-}
-
-
-module.exports.setPOAM_AssetsByCollectionUser = async function setPOAM_AssetsByCollectionUser(req, res, next){
-        res.status(201).json({message: "setPOAM_AssetsByCollectionUser Method called successfully"})
-}
-
-module.exports.getCollectionLabels = async function getCollectionLabels(req, res, next){
-        res.status(201).json({message: "getCollectionLabels Method called successfully"})
-}
-
-
-module.exports.createCollectionLabel = async function createCollectionLabel(req, res, next){
-        res.status(201).json({message: "createCollectionLabel Method called successfully"})
-}
-
-module.exports.getCollectionLabelById = async function getCollectionLabelById(req, res, next){
-        res.status(201).json({message: "getCollectionLabelById Method called successfully"})
 }
 
