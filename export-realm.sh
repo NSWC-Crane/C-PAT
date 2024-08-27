1329592c9122d21933a2a6e96eb9f71647f00a88
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2024-08-27 15:42:58 -0400

    Update

--- export-realm.sh
+++ export-realm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec -it ${1:-c-pat-auth} /opt/keycloak/bin/kc.sh export --dir /tmp --realm RMFTools --users realm_file
+docker exec -it ${1:-c-pat-auth_dev} /opt/keycloak/bin/kc.sh export --dir /tmp --realm RMFTools --users realm_file
 
-docker cp ${1:-c-pat-auth}:/tmp/RMFTools-realm.json .
+docker cp ${1:-c-pat-auth_dev}:/tmp/RMFTools-realm.json .
 
