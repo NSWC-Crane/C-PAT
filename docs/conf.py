dae06aabe144157f265ea9804921f8a32d6c4607
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2025-02-26 11:33:15 -0500

    Docs update

--- docs/conf.py
+++ docs/conf.py
@@ -1,41 +1,48 @@
 import sphinx_rtd_theme
 
+# Project information
 project = 'C-PAT'
-copyright = '2024 U.S. Federal Government (in countries where recognized)'
+copyright = '2025 U.S. Federal Government (in countries where recognized)'
 author = 'Christian Rodriguez'
-
 release = '1.0.0-rc.8'
 version = '1.0.0-rc.8'
 
-html_css_files = [
-    'css/custom.css',
-]
-
+# Extensions
 extensions = [
     'sphinx_rtd_theme',
+    'sphinxcontrib.images',
     'sphinx.ext.todo',
     'myst_parser',
-    'sphinx_tabs.tabs',
-    'sphinxcontrib.images',
+    'sphinx_tabs.tabs'
 ]
-html_logo = 'assets/images/cpat.svg'
+
+# Todo configuration
+todo_include_todos = True
+
+# Images configuration
 images_config = {
     'override_image_directive': True,
     'default_image_width': '50%',
     'default_group': 'default'
 }
 
+# Path configuration
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+github_doc_root = 'https://github.com/NSWC-Crane/C-PAT/tree/main/docs'
+html_static_path = ['_static']
+html_output_dir = '_build/html'
 
+# Theme configuration
 html_theme = "sphinx_rtd_theme"
-
+html_logo = 'assets/images/cpat.svg'
 html_theme_options = {
     'logo_only': True,
     'prev_next_buttons_location': 'both',
     'sticky_navigation': True
 }
+
+# Additional HTML configuration
+html_css_files = ['css/custom.css']
 html_show_sphinx = False
-html_static_path = ['_static']
-html_output_dir = '_build/html'
 epub_show_urls = 'footnote'
