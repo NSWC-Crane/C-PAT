ad8baa85dcc5f2bdd9cd8d208f51869b2b568e1d
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2025-04-24 10:46:25 -0400

    Umzug V3 upgrade

--- api/Services/migrations/lib/umzug-mysql-storage.js
+++ api/Services/migrations/lib/umzug-mysql-storage.js
@@ -9,41 +9,85 @@
 */
 
 const config = require('../../../utils/config');
+const logger = require('../../../utils/logger');
 
-module.exports = class MyStorage {
-    constructor(options) {
-        this.pool = options.pool
-        this.hasMigrationTable = false
+module.exports = class MySqlStorage {
+  constructor({ pool }) {
+    if (!pool) {
+      throw new Error("MySQLStorage requires a pool instance.");
     }
+    this.pool = pool;
+    this.hasMigrationTable = false;
+  }
 
-    async createMigrationTable() {
-        await this.pool.query(`CREATE TABLE IF NOT EXISTS ${config.database.schema}._migrations (
+  async _ensureMigrationTable() {
+    if (!this.hasMigrationTable) {
+      try {
+        await this.pool
+          .query(`CREATE TABLE IF NOT EXISTS ${config.database.schema}._migrations (
       createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
       updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP, 
       name VARCHAR(128) 
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`)
-        this.hasMigrationTable = true
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`);
+        this.hasMigrationTable = true;
+      } catch (error) {
+        logger.writeError("umzug-storage", "_ensureMigrationTable error", {
+          error: error.message,
+          stack: error.stack,
+        });
+        throw error;
+      }
     }
+  }
 
-    async logMigration(migrationName) {
-        if (!this.hasMigrationTable) {
-            await this.createMigrationTable()
-        }
-        await this.pool.query(`INSERT into ${config.database.schema}._migrations (name) VALUES (?)`, [migrationName])
+  async logMigration({ name }) {
+    await this._ensureMigrationTable();
+    try {
+      await this.pool.query(
+        `INSERT into ${config.database.schema}._migrations (name) VALUES (?) ON DUPLICATE KEY UPDATE name=name`,
+        [name]
+      );
+    } catch (error) {
+      logger.writeError("umzug-storage", "logMigration error", {
+        name,
+        error: error.message,
+        stack: error.stack,
+      });
+      throw error;
     }
+  }
 
-    async unlogMigration(migrationName) {
-        if (!this.hasMigrationTable) {
-            await this.createMigrationTable()
-        }
-        await this.pool.query(`DELETE from ${config.database.schema}._migrations WHERE name = ?`, [migrationName])
+  async unlogMigration({ name }) {
+    await this._ensureMigrationTable();
+    try {
+      await this.pool.query(
+        `DELETE from ${config.database.schema}._migrations WHERE name = ?`,
+        [name]
+      );
+    } catch (error) {
+      logger.writeError("umzug-storage", "unlogMigration error", {
+        name,
+        error: error.message,
+        stack: error.stack,
+      });
+      throw error;
     }
+  }
 
-    async executed() {
-        if (!this.hasMigrationTable) {
-            await this.createMigrationTable()
-        }
-        let [rows] = await this.pool.query(`SELECT name from ${config.database.schema}._migrations`)
-        return rows.map(r => r.name)
+  async executed() {
+    let executedNames = [];
+    try {
+      await this._ensureMigrationTable();
+      const [rows] = await this.pool.query(
+        `SELECT name from ${config.database.schema}._migrations ORDER BY name ASC`
+      );
+      executedNames = rows.map((r) => r.name);
+    } catch (error) {
+      logger.writeError("umzug-storage", "executed error", {
+        error: error.message,
+        stack: error.stack,
+      });
     }
-}
+    return executedNames;
+  }
+};
--- api/Services/utils.js
+++ api/Services/utils.js
@@ -12,39 +12,46 @@ const mysql = require('mysql2/promise');
 const config = require('../utils/config');
 const logger = require('../utils/logger');
 const retry = require('async-retry');
-const Umzug = require('umzug');
+const { Umzug } = require('umzug');
 const path = require('path');
-const fs = require("fs");
+const fs = require('fs');
 const semverLt = require('semver/functions/lt');
 const semverCoerce = require('semver/functions/coerce');
 const Importer = require('./migrations/lib/mysql-import.js');
+const MySqlStorage = require('./migrations/lib/umzug-mysql-storage');
 const minMySqlVersion = '8.0.21';
 let _this = this;
 let initAttempt = 0;
 
 module.exports.testConnection = async function () {
-    logger.writeDebug('mysql', 'preflight', { attempt: ++initAttempt })
-    let [result] = await _this.pool.query('SELECT VERSION() as version')
-    let [tables] = await _this.pool.query('SHOW TABLES')
+    logger.writeDebug('mysql', 'preflight', { attempt: ++initAttempt });
+    let [result] = await _this.pool.query('SELECT VERSION() as version');
+    let [tables] = await _this.pool.query('SHOW TABLES');
     return {
         detectedMySqlVersion: result[0].version,
-        detectedTables: tables.length
-    }
-}
+        detectedTables: tables.length,
+    };
+};
 
 async function setupInitialDatabase(pool) {
-    const importer = new Importer(pool)
-    const dir = path.join(__dirname, 'migrations', 'sql', 'current')
-    const files = await fs.promises.readdir(dir)
+    const importer = new Importer(pool);
+    const dir = path.join(__dirname, 'migrations', 'sql', 'current');
+    const files = await fs.promises.readdir(dir);
     try {
         for (const file of files) {
-            logger.writeInfo('mysql', 'initalizing', { status: 'running', name: file })
-            await importer.import(path.join(dir, file))
+            logger.writeInfo('mysql', 'initalizing', {
+                status: 'running',
+                name: file,
+            });
+            await importer.import(path.join(dir, file));
         }
-    }
-    catch (e) {
-        logger.writeError('mysql', 'initialize', { status: 'error', files: files, message: e.message })
-        throw new Error(`Failed to initialize database with file ${e.message}`)
+    } catch (e) {
+        logger.writeError('mysql', 'initialize', {
+            status: 'error',
+            files: files,
+            message: e.message,
+        });
+        throw new Error(`Failed to initialize database with file ${e.message}`);
     }
 }
 
@@ -59,35 +66,44 @@ function getPoolConfig() {
         decimalNumbers: true,
         charset: 'utf8mb4_0900_ai_ci',
         typeCast: function (field, next) {
-            if ((field.type === "BIT") && (field.length === 1)) {
-                let bytes = field.buffer() || [0]
-                return (bytes[0] === 1)
+            if (field.type === 'BIT' && field.length === 1) {
+                let bytes = field.buffer() || [0];
+                return bytes[0] === 1;
             }
-            return next()
-        }
-    }
+            return next();
+        },
+    };
     if (config.database.password) {
-        poolConfig.password = config.database.password
+        poolConfig.password = config.database.password;
     }
-    if (config.database.tls.ca_file || config.database.tls.cert_file || config.database.tls.key_file) {
-        const sslConfig = {}
+    if (
+        config.database.tls.ca_file ||
+        config.database.tls.cert_file ||
+        config.database.tls.key_file
+    ) {
+        const sslConfig = {};
         if (config.database.tls.ca_file) {
-            sslConfig.ca = fs.readFileSync(path.join(__dirname, '..', 'tls', config.database.tls.ca_file))
+            sslConfig.ca = fs.readFileSync(
+                path.join(__dirname, '..', 'tls', config.database.tls.ca_file)
+            );
         }
         if (config.database.tls.cert_file) {
-            sslConfig.cert = fs.readFileSync(path.join(__dirname, '..', 'tls', config.database.tls.cert_file))
+            sslConfig.cert = fs.readFileSync(
+                path.join(__dirname, '..', 'tls', config.database.tls.cert_file)
+            );
         }
         if (config.database.tls.key_file) {
-            sslConfig.key = fs.readFileSync(path.join(__dirname, '..', 'tls', config.database.tls.key_file))
+            sslConfig.key = fs.readFileSync(
+                path.join(__dirname, '..', 'tls', config.database.tls.key_file)
+            );
         }
-        poolConfig.ssl = sslConfig
+        poolConfig.ssl = sslConfig;
     }
-    return poolConfig
+    return poolConfig;
 }
 
 module.exports.initializeDatabase = async function (depStatus) {
     const poolConfig = getPoolConfig();
-    logger.writeDebug('mysql', 'poolConfig', { ...poolConfig });
     _this.pool = mysql.createPool(poolConfig);
     module.exports.pool = _this.pool;
     _this.pool.on('connection', function (connection) {
@@ -101,7 +117,10 @@ module.exports.initializeDatabase = async function (depStatus) {
             logger.writeInfo('mysql', 'close', { success: true });
             process.exit(0);
         } catch (err) {
-            logger.writeError('mysql', 'close', { success: false, message: err.message });
+            logger.writeError('mysql', 'close', {
+                success: false,
+                message: err.message,
+            });
             process.exit(1);
         }
     }
@@ -110,127 +129,210 @@ module.exports.initializeDatabase = async function (depStatus) {
     process.on('SIGTERM', closePoolAndExit);
     process.on('SIGINT', closePoolAndExit);
 
-    const { detectedTables, detectedMySqlVersion } = await retry(_this.testConnection, {
-        retries: 24,
-        factor: 1,
-        minTimeout: 5 * 1000,
-        maxTimeout: 5 * 1000,
-        onRetry: (error) => {
-            logger.writeError('mysql', 'preflight', { success: false, message: error.message })
+    const { detectedTables, detectedMySqlVersion } = await retry(
+        _this.testConnection,
+        {
+            retries: 24,
+            factor: 1,
+            minTimeout: 5 * 1000,
+            maxTimeout: 5 * 1000,
+            onRetry: (error) => {
+                logger.writeError('mysql', 'preflight', {
+                    success: false,
+                    message: error.message,
+                });
+            },
         }
-    })
+    );
     if (semverLt(semverCoerce(detectedMySqlVersion), minMySqlVersion)) {
-        logger.writeError('mysql', 'preflight', { success: false, message: `MySQL release ${detectedMySqlVersion} is too old. Update to release ${minMySqlVersion} or later.` })
-        process.exit(1)
-    }
-    else {
+        logger.writeError('mysql', 'preflight', {
+            success: false,
+            message: `MySQL release ${detectedMySqlVersion} is too old. Update to release ${minMySqlVersion} or later.`,
+        });
+        process.exit(1);
+    } else {
         logger.writeInfo('mysql', 'preflight', {
             success: true,
-            version: detectedMySqlVersion
-        })
+            version: detectedMySqlVersion,
+        });
     }
 
     try {
         if (detectedTables === 0) {
-            logger.writeInfo('mysql', 'setup', { message: 'No existing tables detected. Setting up new database.' })
-            await setupInitialDatabase(_this.pool)
-            logger.writeInfo('mysql', 'setup', { message: 'Database setup complete.' })
+            logger.writeInfo('mysql', 'setup', {
+                message: 'No existing tables detected. Setting up new database.',
+            });
+            await setupInitialDatabase(_this.pool);
+            logger.writeInfo('mysql', 'setup', {
+                message: 'Database setup complete.',
+            });
         }
+
+        const storage = new MySqlStorage({ pool: _this.pool });
+        const umzugGlobPattern = path
+            .join(__dirname, './migrations/*.js')
+            .replace(/\\/g, '/');
 
         const umzug = new Umzug({
             migrations: {
-                path: path.join(__dirname, './migrations'),
-                params: [_this.pool]
+                glob: umzugGlobPattern,
+                resolve: ({ name, path: migrationPath, context }) => {
+                    const migration = require(migrationPath);
+                    return {
+                        name: name,
+                        up: async ({ context }) => migration.up(context, name),
+                        down: async ({ context }) => migration.down(context, name),
+                    };
+                },
             },
-            storage: path.join(__dirname, './migrations/lib/umzug-mysql-storage'),
-            storageOptions: {
-                pool: _this.pool
-            }
-        })
+            storage: storage,
+            context: _this.pool,
+            logger: {
+                info: (message) =>
+                    logger.writeInfo(
+                        'umzug',
+                        'info',
+                        typeof message === 'object' ? message : { message }
+                    ),
+                warn: (message) =>
+                    logger.writeWarn(
+                        'umzug',
+                        'warn',
+                        typeof message === 'object' ? message : { message }
+                    ),
+                error: (error) =>
+                    logger.writeError(
+                        'umzug',
+                        'error',
+                        typeof error === 'object' ? error : { message: error }
+                    ),
+                debug: (message) =>
+                    logger.writeDebug(
+                        'umzug',
+                        'debug',
+                        typeof message === 'object' ? message : { message }
+                    ),
+            },
+            mainScript: require.main === module ? __filename : undefined,
+        });
+
+        const pendingMigrations = await umzug.pending();
 
         if (config.database.revert) {
-            const migrations = await umzug.executed()
-            if (migrations.length) {
-                logger.writeInfo('mysql', 'migration', { message: 'MySQL schema will revert the last migration and terminate' })
-                await umzug.down()
+            const executedMigrations = await umzug.executed();
+            if (executedMigrations.length) {
+                logger.writeInfo('mysql', 'migration', {
+                    message: 'MySQL schema will revert the last migration and terminate',
+                });
+                await umzug.down();
             } else {
-                logger.writeInfo('mysql', 'migration', { message: 'MySQL schema has no migrations to revert' })
+                logger.writeInfo('mysql', 'migration', {
+                    message: 'MySQL schema has no migrations to revert',
+                });
             }
-            logger.writeInfo('mysql', 'migration', { message: 'MySQL revert migration has completed' })
-            process.exit(1)
+            logger.writeInfo('mysql', 'migration', {
+                message: 'MySQL revert migration has completed',
+            });
+            process.exit(1);
         }
-        const migrations = await umzug.pending()
-        if (migrations.length > 0) {
-            logger.writeInfo('mysql', 'migration', { message: `MySQL schema requires ${migrations.length} update${migrations.length > 1 ? 's' : ''}` })
-            await umzug.up()
-            logger.writeInfo('mysql', 'migration', { message: `All migrations performed successfully` })
+
+        if (pendingMigrations.length > 0) {
+            logger.writeInfo('mysql', 'migration', {
+                message: `MySQL schema requires ${pendingMigrations.length} update${pendingMigrations.length > 1 ? 's' : ''
+                    }`,
+                migrations: pendingMigrations.map((m) => m.name),
+            });
+            await umzug.up();
+            logger.writeInfo('mysql', 'migration', {
+                message: `All migrations performed successfully`,
+            });
+        } else {
+            logger.writeInfo('mysql', 'migration', {
+                message: `MySQL schema is up to date`,
+            });
         }
-        else {
-            logger.writeInfo('mysql', 'migration', { message: `MySQL schema is up to date` })
+        depStatus.db = 'up';
+        const migrated = await umzug.executed();
+
+        if (migrated.length > 0) {
+            const lastName = migrated[migrated.length - 1].name;
+            config.lastMigration = parseInt(
+                lastName.replace('.js', '').substring(0, 4)
+            );
+        } else {
+            config.lastMigration = -1;
         }
-        depStatus.db = 'up'
-        const migrated = await umzug.executed()
-        config.lastMigration = parseInt(migrated[migrated.length - 1].file.substring(0, 4))
+    } catch (error) {
+        logger.writeError('mysql', 'initalization', {
+            message: error.message,
+            stack: error.stack,
+        });
+        depStatus.db = 'failed';
+        throw new Error('Failed during database initialization or migration.');
     }
-    catch (error) {
-        logger.writeError('mysql', 'initalization', { message: error.message })
-        depStatus.db = 'failed'
-        throw new Error('Failed during database initialization or migration.')
-    }
-}
+};
 
 module.exports.uuidToSqlString = function (uuid) {
     return {
         toSqlString: function () {
-            return `UUID_TO_BIN(${mysql.escape(uuid)},1)`
-        }
-    }
-}
+            return `UUID_TO_BIN(${mysql.escape(uuid)},1)`;
+        },
+    };
+};
 
-module.exports.makeQueryString = function ({ ctes = [], columns, joins, predicates, groupBy, orderBy }) {
+module.exports.makeQueryString = function ({
+    ctes = [],
+    columns,
+    joins,
+    predicates,
+    groupBy,
+    orderBy,
+}) {
     const query = `
 ${ctes.length ? 'WITH ' + ctes.join(',  \n') : ''}
 SELECT
   ${columns.join(',\n  ')}
 FROM
   ${joins.join('\n  ')}
-${predicates?.statements.length ? 'WHERE\n  ' + predicates.statements.join(' and\n  ') : ''}
+${predicates?.statements.length
+            ? 'WHERE\n  ' + predicates.statements.join(' and\n  ')
+            : ''
+        }
 ${groupBy?.length ? 'GROUP BY\n  ' + groupBy.join(',\n  ') : ''}
 ${orderBy?.length ? 'ORDER BY\n  ' + orderBy.join(',\n  ') : ''}
-`
-    return query
-}
+`;
+    return query;
+};
 
-module.exports.CONTEXT_ALL = 'all'
-module.exports.CONTEXT_DEPT = 'department'
-module.exports.CONTEXT_USER = 'user'
+module.exports.CONTEXT_ALL = 'all';
+module.exports.CONTEXT_DEPT = 'department';
+module.exports.CONTEXT_USER = 'user';
 
 module.exports.WRITE_ACTION = {
     CREATE: 0,
     REPLACE: 1,
-    UPDATE: 2
-}
+    UPDATE: 2,
+};
 
 module.exports.retryOnDeadlock = async function (fn, statusObj = {}) {
     const retryFunction = async function (bail) {
         try {
-            return await fn()
-        }
-        catch (e) {
+            return await fn();
+        } catch (e) {
             if (e.code === 'ER_LOCK_DEADLOCK') {
-                throw (e)
+                throw e;
             }
-            bail(e)
+            bail(e);
         }
-    }
-    statusObj.retries = 0
+    };
+    statusObj.retries = 0;
     return await retry(retryFunction, {
         retries: 15,
         factor: 1,
         minTimeout: 200,
         maxTimeout: 200,
         onRetry: () => {
-            ++statusObj.retries
-        }
-    })
-}
+            ++statusObj.retries;
+        },
+    });
+};
