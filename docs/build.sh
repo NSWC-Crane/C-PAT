9a68d65af9c7df0287437e60d38cf683656dcd41
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2026-09-04 11:20:49 -0400

    fix(build): Use double brackets for conditional check in build script

--- docs/build.sh
+++ docs/build.sh
@@ -5,9 +5,9 @@ SPHINX_IMAGE_W_REQUIREMENTS=sphinx-w-requirements
 # Change to this script directory
 cd "$(dirname "$(realpath "$0")")"
 
-if [ "$1" = "outdated" ]; then
-    [ -d .venv ] || uv venv .venv --python 3.12
-    uv pip sync -q requirements.txt --python .venv && uv pip list --outdated --python .venv
+if [[ "$1" = "outdated" ]]; then
+    [[ -d .venv ]] || uv venv .venv --python 3.12
+    uv pip sync -q requirements.txt --no-build --python .venv && uv pip list --outdated --python .venv
     exit $?
 fi
 
